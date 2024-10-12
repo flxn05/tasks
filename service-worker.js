c0ed6fd16cc6e31f42e0d7ff5f5bc7a45a62c009
flxn05 <163238790+flxn05@users.noreply.github.com>
2024-10-12 17:13:44 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const options = {
+      body: data.body,
+      icon: 'icon.png', // You can provide your custom icon
+      badge: 'badge.png', // You can provide a badge image
+  };
+
+  event.waitUntil(
+      self.registration.showNotification(data.title, options)
+  );
+});
